ea0cecd30e27223d575863eb9d6737663df62125
Chintan Ganeshwala <chinta.ganeshwala@gmail.com>
2021-10-12 11:03:19 -0400

    Insertion of new node at any position

--- src/Com/DS/LinkedList/CircularSinglyLinkedList.java
+++ src/Com/DS/LinkedList/CircularSinglyLinkedList.java
@@ -18,4 +18,37 @@ public class CircularSinglyLinkedList {
 		size=1;
 		return head;
 	}
+	
+	public void insertNode(int newNodeValue,int location) {
+		Node node= new Node();
+		node.value=newNodeValue;
+		if(head==null) {
+			createCircularSinglyLinkedList(newNodeValue);
+			return;
+		}
+		else if(location==0) {
+			node.next=head;
+			head=node;
+			tail.next=node;
+			System.out.println("Node is create at first position");
+		}
+		else if(location >=size) {
+			tail.next=node;
+			tail=node;
+			tail.next=head; // node.next=head 
+			System.out.println("Node create at last position");
+		}
+		else {
+			Node tempNode = new Node();
+			tempNode=head;
+			int index=0;
+			while(index<location-1) {
+				tempNode=tempNode.next;
+				index++;
+			}
+			node.next=tempNode.next;
+			tempNode.next=node;
+		}
+		size++;
+	}
 }
--- src/Com/DS/LinkedList/CircularSinglyLinkedListMain.java
+++ src/Com/DS/LinkedList/CircularSinglyLinkedListMain.java
@@ -8,6 +8,25 @@ public class CircularSinglyLinkedListMain {
 		csll.createCircularSinglyLinkedList(5);
 		System.out.println(csll.head.value);
 		System.out.println(csll.head.next.value);
+		
+		// intsertion new node value at first
+		csll.insertNode(4, 0);
+		System.out.println(csll.head.value);
+		System.out.println(csll.head.next.value);
+		//System.out.println(csll.tail.next.value);
+		
+		// intsertion new node value at last
+		csll.insertNode(8, 3);
+		System.out.println(csll.tail.value);
+		System.out.println(csll.head.next.value);
+		//System.out.println(csll.tail.next.value);
+
+		// intsertion new node value at any position
+		csll.insertNode(6,2);
+		System.out.println(csll.tail.value);
+		System.out.println(csll.head.next.value);
+		System.out.println(csll.tail.next.value);
+		
 	}
 
 }
