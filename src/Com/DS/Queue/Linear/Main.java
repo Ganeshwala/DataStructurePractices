bd77e6794c0a3ecb73f8a0fbeff356b4b9b1b33c
Chintan Ganeshwala <chinta.ganeshwala@gmail.com>
2021-10-18 13:47:32 -0400

    implementation

---
+++ src/Com/DS/Queue/Linear/Main.java
@@ -0,0 +1,18 @@
+package Com.DS.Queue.Linear;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		QueueArray queue = new QueueArray(4);
+		queue.isEmpty();
+		queue.isFull();
+		queue.enQueue(5);
+		queue.enQueue(10);
+		queue.enQueue(1);
+		queue.enQueue(3);
+		System.out.println("first element of"+queue.deQueue());
+		System.out.println("Second element of Queue"+queue.deQueue());
+	}
+
+}
--- src/Com/DS/Queue/Linear/QueueArray.java
+++ src/Com/DS/Queue/Linear/QueueArray.java
@@ -8,5 +8,44 @@ public class QueueArray {
 	
 	public QueueArray(int size){
 		arr=new int[size];
+		topOfQueue=-1;
+		beginningOfQueue=-1;
+	}
+	
+	public boolean isEmpty() {
+		if(topOfQueue==-1) {
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean isFull() {
+		if(topOfQueue==arr.length-1) {
+			return true;
+		}
+		return false;
+	}
+	
+	public void enQueue(int value) {
+		if(isFull()) {
+			return ;
+		}else if(isEmpty()) {
+			beginningOfQueue=0;
+		}
+		topOfQueue++;
+		arr[topOfQueue]=value;
+	}
+	public int deQueue() {
+		if(isEmpty()) {
+			return -1;
+		}
+		else {
+			int removeElement = arr[beginningOfQueue];
+			beginningOfQueue++;
+			if(beginningOfQueue>topOfQueue) {
+				beginningOfQueue=topOfQueue=-1;
+			}
+			return removeElement;
+		}
 	}
 }
