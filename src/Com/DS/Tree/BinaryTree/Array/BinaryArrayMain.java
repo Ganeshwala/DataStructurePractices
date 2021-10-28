66651a19d64cb7688811c3b6dc2e52e2baa1ed51
Chintan Ganeshwala <chinta.ganeshwala@gmail.com>
2021-10-28 00:29:41 -0400

    Binary Tree using Array

---
+++ src/Com/DS/Tree/BinaryTree/Array/BinaryArrayMain.java
@@ -0,0 +1,11 @@
+package Com.DS.Tree.BinaryTree.Array;
+
+public class BinaryArrayMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		BinaryTree bt = new BinaryTree(5);
+		
+	}
+
+}
---
+++ src/Com/DS/Tree/BinaryTree/Array/BinaryTree.java
@@ -0,0 +1,13 @@
+package Com.DS.Tree.BinaryTree.Array;
+
+public class BinaryTree {
+
+	String[] arr;
+	int lastIndex;
+	
+	public BinaryTree(int size) {
+		arr = new String[size];
+		this.lastIndex = 0;
+		System.out.println("Blank Tree of size "+size+" is Created !!!!");
+	}
+}
