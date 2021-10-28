dcec3e3273554e304048e1960aa11cd5817b5263
Chintan Ganeshwala <chinta.ganeshwala@gmail.com>
2021-10-28 00:43:27 -0400

    Insert Method Implementation

--- src/Com/DS/Tree/BinaryTree/Array/BinaryArrayMain.java
+++ src/Com/DS/Tree/BinaryTree/Array/BinaryArrayMain.java
@@ -5,6 +5,11 @@ public class BinaryArrayMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		BinaryTree bt = new BinaryTree(5);
+		bt.InsertNode("N1");
+		bt.InsertNode("N3");
+		bt.InsertNode("N4");
+		bt.InsertNode("N5");
+		bt.InsertNode("N2");
 		
 	}
 
--- src/Com/DS/Tree/BinaryTree/Array/BinaryTree.java
+++ src/Com/DS/Tree/BinaryTree/Array/BinaryTree.java
@@ -7,11 +7,25 @@ public class BinaryTree {
 	
 	public BinaryTree(int size) {
 		arr = new String[size];
-		this.lastIndex = 0;
+		this.lastIndex = -1;
 		System.out.println("Blank Tree of size "+size+" is Created !!!!");
 	}
 	
-	public BinaryTree InsertNode(String value){
+	public boolean isFull() {
+		if(arr.length-1 == lastIndex) {
+			return true;
+		}else {
+			return false;
+		}
+	}
 	
+	public void InsertNode(String value){
+		if(!isFull()) {
+			arr[lastIndex+1] = value;
+			lastIndex++;
+			System.out.println("Value "+value+" has been Inserted !!");
+		}else {
+			System.out.println("Binary tree is Full");
+		}
 	}
 }
