993a0ea068e8ac869d589d6bda6e1b003dfb5c2a
Chintan Ganeshwala <chinta.ganeshwala@gmail.com>
2021-10-29 14:09:22 -0400

    Insert value into BST

---
+++ src/Com/DS/Tree/BinarySearchTree/BSTMain.java
@@ -0,0 +1,22 @@
+package Com.DS.Tree.BinarySearchTree;
+
+public class BSTMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TreeNode node = new TreeNode();
+		node.InsertNode(5);
+		node.InsertNode(8);
+		node.InsertNode(3);
+		node.InsertNode(6);
+		node.InsertNode(7);
+		node.InsertNode(4);
+		node.InsertNode(10);
+		node.InsertNode(2);
+		node.InsertNode(9);
+		node.InsertNode(0);
+		node.InsertNode(1);
+		node.InsertNode(5);
+	}
+
+}
---
+++ src/Com/DS/Tree/BinarySearchTree/TreeNode.java
@@ -0,0 +1,53 @@
+package Com.DS.Tree.BinarySearchTree;
+
+public class TreeNode {
+
+	public int nodeValue;
+	TreeNode leftNode,rightNode,root;
+	
+	public TreeNode() {
+		root = null;
+	}
+	
+	public void InsertValueBST(TreeNode currentNode,int value) {
+		TreeNode newNode = new TreeNode();
+		newNode.nodeValue = value;
+		if(root == null) {
+			root = newNode;
+			System.out.println("Root node is added");
+		}else {
+			//System.out.println(currentNode.nodeValue);
+			if(currentNode.nodeValue >value ) {
+				if(currentNode.leftNode !=null) {
+					currentNode = currentNode.leftNode;
+					InsertValueBST(currentNode,value);
+				}
+				else {
+					currentNode.leftNode = newNode;
+					System.out.println("Left Node is Added");
+				}
+			}else if(currentNode.nodeValue < value){
+				if(currentNode.rightNode != null) {
+					currentNode = currentNode.rightNode;
+					InsertValueBST(currentNode,value);
+				}else {
+					currentNode.rightNode = newNode;
+					System.out.println("Right node is added");
+				}
+			}else {
+				System.out.println("Node all ready there !!!");
+			}
+		}
+	}
+	
+	public void InsertNode(int value) {
+		InsertValueBST(root, value);
+	}
+	
+	public void SearchValue(TreeNode currentNode,int value) {
+		if(root == null) {
+			System.out.println();
+		}
+	}
+	
+}
