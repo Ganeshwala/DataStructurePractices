74f13cc2064152a75d304ce1222171caeb97d95c
Chintan Ganeshwala <69555721+Ganeshwala@users.noreply.github.com>
2021-10-28 23:07:31 -0400

    Update BinaryArrayMain.java
    Implementation of Traversal method of tree using Array.

--- src/Com/DS/Tree/BinaryTree/Array/BinaryArrayMain.java
+++ src/Com/DS/Tree/BinaryTree/Array/BinaryArrayMain.java
@@ -10,12 +10,16 @@ public class BinaryArrayMain {
 		bt.InsertNode("N4");
 		bt.InsertNode("N5");
 		bt.InsertNode("N2");
+		System.out.println("Pre Order of Binary Tree");
 		bt.preOrderTraversal(1);
 		System.out.println();
+		System.out.println("In Order of Binary Tree");
 		bt.inOrderTraversal(1);
 		System.out.println();
+		System.out.println("Post Order of Binary Tree");
 		bt.postOrderTraversal(1);
 		System.out.println();
+		System.out.println("level Order of Binary Tree");
 		bt.levelOrderTraversal(1);
 	}
 
