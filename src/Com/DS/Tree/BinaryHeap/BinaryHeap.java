db24a20324626267d31b9fea1e28f5cf9b3ae869
Chintan Ganeshwala <chinta.ganeshwala@gmail.com>
2021-11-02 12:38:21 -0400

    Heap Tree implementation

---
+++ src/Com/DS/Tree/BinaryHeap/BinaryHeap.java
@@ -0,0 +1,39 @@
+package Com.DS.Tree.BinaryHeap;
+
+public class BinaryHeap {
+
+	int[] arr;
+	int treeSize;
+	
+	public BinaryHeap(int size) {
+		arr = new int[size+1];
+		treeSize = 0;
+	}
+	
+	public boolean isEmpty() {
+		if(treeSize == 0) {
+			return true;
+		}
+		return false;
+	}
+	
+	public Integer peek() {
+		if(isEmpty()) {
+			System.out.println("Heap tree is Empty");
+			return null;
+		}else {
+			return arr[1];
+		}
+	}
+	
+	public int sizeOfHeap() {
+		return treeSize;
+	}
+	
+	public void levelTraversal() {
+		for(int i=1;i<=treeSize;i++) {
+			System.out.print(arr[i]+" ");
+		}
+		System.out.println();
+	}
+}
---
+++ src/Com/DS/Tree/BinaryHeap/HeapMain.java
@@ -0,0 +1,11 @@
+package Com.DS.Tree.BinaryHeap;
+
+public class HeapMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		BinaryHeap heap = new BinaryHeap(5);
+		heap.peek();
+	}
+
+}
