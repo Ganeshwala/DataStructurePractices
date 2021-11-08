402959a677af1d358696bb8f910089e8fe33f03b
Ganeshwala <ganeshwalachintan@gmail.com>
2021-11-08 10:31:39 -0500

    min Heap

--- src/Com/DS/Tree/BinaryHeap/HeapMain.java
+++ src/Com/DS/Tree/BinaryHeap/HeapMain.java
@@ -5,14 +5,15 @@ public class HeapMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		BinaryHeap heap = new BinaryHeap(5);
-		heap.insertValue(10, "Max");
-		heap.insertValue(5, "Max");
-		heap.insertValue(15, "Max");
-		heap.insertValue(8, "Max");
-		heap.insertValue(1, "Max");
+		heap.insertValue(10, "Min");
+		heap.insertValue(5, "Min");
+		heap.insertValue(15, "Min");
+		heap.insertValue(8, "Min");
+		heap.insertValue(1, "Min");
 		//System.out.println(heap.peek());
 		heap.levelTraversal();
 		System.out.println("======");
+		System.out.println(heap.extractHeadOfHeap("Min"));
 		System.out.println(heap.extractHeadOfHeap("Min"));
 		System.out.println("======");
 		heap.levelTraversal();
